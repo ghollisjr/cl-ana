a6917b10ec4c777beb5c12695e162c63b6d1ee6c
ghollisjr <ghollisjr@gmail.com>
2014-01-28 18:49:28 -0500

    Added my own gnuplot-interface which depends on external-program; will remove gnuplot-i dependency shortly

---
+++ gnuplot-interface/gnuplot-interface.lisp
@@ -0,0 +1,21 @@
+(ql:quickload "external-program")
+
+(defpackage #:gnuplot-interface
+  (:use :cl
+        :external-program))
+
+(in-package :gnuplot-interface)
+
+(defun gnuplot-init ()
+  (start "gnuplot" ()
+         :input :stream))
+
+(defun gnuplot-close (session)
+  (close (process-input-stream session)))
+
+(defun gnuplot-cmd (session command-string)
+  (let ((input-stream (process-input-stream session)))
+    (format input-stream
+            "~a~%"
+            command-string)
+    (finish-output input-stream)))
