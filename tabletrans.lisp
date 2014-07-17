672cb9246aa645e6be8ac13cbbe2e556e3578a21
ghollisjr <ghollisjr@gmail.com>
2014-07-16 23:00:32 -0400

    Comment for tabletrans progress

--- tabletrans.lisp
+++ tabletrans.lisp
@@ -1,10 +1,10 @@
 (in-package :makeres-tabletrans)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun tabletrans (target-table)
-    (makeres::pipe-functions (list #'pass-collapse
-                                   #'pass-merge)
-                             target-table)))
+;; (eval-when (:compile-toplevel :load-toplevel :execute)
+;;   (defun tabletrans (target-table)
+;;     (makeres::pipe-functions (list #'pass-collapse
+;;                                    #'pass-merge)
+;;                              target-table)))
 
 ;;;; New algorithm:
 
