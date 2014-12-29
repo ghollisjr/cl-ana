93ac9bb8d3371baf31a0227a14907a2670ab9185
ghollisjr <ghollisjr@gmail.com>
2014-12-29 18:02:42 -0500

    Actually fixed elisp functions

--- emacs-lisp/makeres.el
+++ emacs-lisp/makeres.el
@@ -1,9 +1,12 @@
 (defun makeres (&rest args)
   "Executes makeres with arguments in current slime buffer."
   (interactive)
-  (let ((cmd "(makeres)"))
-    (slime-write-string cmd)
-    (slime-repl-send-string cmd)))
+  (let ((slime-buffer (slime-repl))
+        (old-buffer (current-buffer)))
+    (switch-to-buffer slime-buffer)
+    (end-of-buffer)
+    (insert "(makeres)")
+    (slime-repl-return)))
 
 (add-hook 'lisp-mode-hook
           '(lambda ()
