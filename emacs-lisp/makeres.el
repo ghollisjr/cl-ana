3accb3cbfcfe4d7da5df35dbadaa7b63efce474e
ghollisjr <ghollisjr@gmail.com>
2015-02-17 14:58:01 -0500

    Added call to makeres-propogate! in emacs lisp function so that lack of makeres-auto-propogate is not annoying.

--- emacs-lisp/makeres.el
+++ emacs-lisp/makeres.el
@@ -1,11 +1,12 @@
 (defun makeres (&rest args)
-  "Executes makeres with arguments in current slime buffer."
+  "Executes makeres-propogate! and makeres with arguments in
+current slime buffer."
   (interactive)
   (let ((slime-buffer (slime-repl))
         (old-buffer (current-buffer)))
     (switch-to-buffer slime-buffer)
     (end-of-buffer)
-    (insert "(makeres)")
+    (insert "(makeres-propogate!)\n(makeres)")
     (slime-repl-return)))
 
 (add-hook 'lisp-mode-hook
