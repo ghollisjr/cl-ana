ccaff04ecddbbfdc32ebde2f56f82cd43e99ff99
ghollisjr <ghollisjr@gmail.com>
2015-09-09 03:39:13 -0400

    Added install file and worked on documentation

---
+++ install.lisp
@@ -0,0 +1,11 @@
+(require 'quicklisp)
+
+(ql:quickload "cffi")
+(ql:quickload "gsll")
+(ql:quickload "alexandria")
+(ql:quickload "iterate")
+(ql:quickload "antik")
+(ql:quickload "closer-mop")
+(ql:quickload "cl-csv")
+(ql:quickload "cl-fad")
+(ql:quickload "external-program")
